3cb2644fce77bdbd3b4018d5855571415aef41af
KIT <kit311290@gmail.com>
2026-01-21 13:40:45 +0300

    prototype of the "Drag & Drop" mechanics

---
+++ Assets/MiniMergeUI/Scripts/Tests/Cell.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace MiniMergeUI.Tests
+{
+    public  class Cell : MonoBehaviour
+    {
+       private RectTransform _rectTransform;
+        public RectTransform RectTransform { get => _rectTransform; }
+
+        private void Awake()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+        }
+    }
+}

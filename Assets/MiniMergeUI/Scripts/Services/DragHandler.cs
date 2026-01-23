189bf69bec14a4f3dd4972588939a3f3c987daa9
KIT <kit311290@gmail.com>
2026-01-23 08:31:43 +0300

    switched to Android

--- Assets/MiniMergeUI/Scripts/Services/DragHandler.cs
+++ Assets/MiniMergeUI/Scripts/Services/DragHandler.cs
@@ -6,42 +6,100 @@ namespace MiniMergeUI.Services
     public class DragHandler : MonoBehaviour
     {
         public event Action<Vector2> DragStarted;
-        public event Action<Vector2> DragFinished;
         public event Action<Vector2> DragOngoing;
+        public event Action<Vector2> DragFinished;
 
-        private Vector3 _lastMousePosition;
+        [SerializeField] private bool _sendOngoingEveryFrame = false;
 
+        private bool _isDragging;
+        private Vector2 _lastPos;
+        private bool _hasLastPos;
+
+        private int _activeTouchId = -1;
 
         private void Update()
         {
-            if (Input.GetMouseButtonDown(0))
-                StartDrag();
+            GetPointerState(out var pos, out var down, out var held, out var up);
 
-            if (Input.GetMouseButtonUp(0))
-                StopDrag();
+            if (down && !_isDragging)
+                Begin(pos);
 
-            DragIfMouseMoved();
-        }
+            if (!_isDragging)
+                return;
 
-        private void StartDrag()
-        {
-            DragStarted?.Invoke(Input.mousePosition);
-            _lastMousePosition = Input.mousePosition;
-        }
-
-        private void StopDrag()
-        {
-            DragFinished?.Invoke(Input.mousePosition);
-            _lastMousePosition = Input.mousePosition;
-        }
-
-        private void DragIfMouseMoved()
-        {
-            if (Input.mousePosition != _lastMousePosition)
+            if (up)
             {
-                DragOngoing?.Invoke(Input.mousePosition);
-                _lastMousePosition = Input.mousePosition;
+                End(pos);
+                return;
             }
+
+            if (!held && !down)
+            {
+                End(pos);
+                return;
+            }
+
+            if (_sendOngoingEveryFrame || !_hasLastPos || (pos - _lastPos).sqrMagnitude > 0.01f)
+            {
+                DragOngoing?.Invoke(pos);
+                _lastPos = pos;
+                _hasLastPos = true;
+            }
+        }
+
+        private void Begin(Vector2 pos)
+        {
+            _isDragging = true;
+            _lastPos = pos;
+            _hasLastPos = true;
+            DragStarted?.Invoke(pos);
+        }
+
+        private void End(Vector2 pos)
+        {
+            _isDragging = false;
+            _activeTouchId = -1;
+            _hasLastPos = false;
+            DragFinished?.Invoke(pos);
+        }
+
+        private void GetPointerState(out Vector2 pos, out bool down, out bool held, out bool up)
+        {
+            if (Input.touchCount > 0)
+            {
+                Touch t = GetActiveTouch();
+
+                pos = t.position;
+                down = t.phase == TouchPhase.Began;
+                up = t.phase == TouchPhase.Ended || t.phase == TouchPhase.Canceled;
+
+                held = down || t.phase == TouchPhase.Moved || t.phase == TouchPhase.Stationary;
+
+                if (down) _activeTouchId = t.fingerId;
+                if (up) _activeTouchId = -1;
+
+                return;
+            }
+
+            pos = Input.mousePosition;
+            down = Input.GetMouseButtonDown(0);
+            held = Input.GetMouseButton(0);
+            up = Input.GetMouseButtonUp(0);
+        }
+
+        private Touch GetActiveTouch()
+        {
+            if (_activeTouchId == -1)
+                return Input.GetTouch(0);
+
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                var t = Input.GetTouch(i);
+                if (t.fingerId == _activeTouchId)
+                    return t;
+            }
+
+            return Input.GetTouch(0);
         }
     }
 }
